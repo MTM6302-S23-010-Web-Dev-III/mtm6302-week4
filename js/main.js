2a13947cc1e92b0addb293d4faaadf36a8a10943
Abhay Sharma <abhaysharmawebd@gmail.com>
2023-06-02 11:22:22 -0400

    completes demo for week 4

---
+++ js/main.js
@@ -0,0 +1,101 @@
+console.log("JS File Linked!")
+
+const region = "Canada"
+const province = "Alberta"
+
+if (region === "Canada" || province === "Ontario") {
+  console.log("You are seeing Canadian version")
+}
+
+let price = 20
+const shippingFee = 15
+let cartTotal
+
+if (price >= 50) {
+  cartTotal = price
+} else {
+  cartTotal = price + shippingFee
+}
+document.getElementById("cart1").innerHTML += "Your cart total is " + cartTotal
+
+// resetting the price
+price = 105
+const discount = 0.15
+
+if (price >= 100) {
+  cartTotal = price - (price * discount)
+} else if (price >= 50) {
+  cartTotal = price
+} else {
+  cartTotal = price + shippingFee
+}
+document.getElementById("cart2").innerHTML += "Your cart total is " + cartTotal
+
+// Switch
+let planet = "Saturn"
+let message
+
+switch (planet) {
+  case "Earth":
+    message = "Welcome to the third planet!"
+    break
+  case "Mars":
+    message = "Welcome to the red planet!"
+    break
+  case "Jupiter":
+    message = "Welcome to the largest planet!"
+    break
+  default:
+    message = `Oh! You are on ${planet}, sorry we do not service this planet yet!`
+}
+document.getElementById("planet").innerHTML += message
+
+// Switch Ex 2 with fall though
+let answer = "b"
+switch (answer) {
+  case "a":
+  case "c":
+  case "d":
+    document.getElementById("quiz").innerHTML += "Incorrect answer, please try again!"
+    break
+  case "b":
+    document.getElementById("quiz").innerHTML += "Prefect, you got it right!"
+}
+
+/* Functions */
+function myName () {
+  return "Abhay"
+}
+let firstName = myName()
+
+document.querySelector(".container h1").innerHTML += "! Hello " + firstName;
+
+/* Performing a given operation on two given numbers */
+
+function calculator (a, b, operator) {
+  let result
+  switch (operator) {
+    case "add":
+      result = a + b
+      break
+    case "sub":
+      result = a - b
+      break
+    case "multi":
+      result = a * b
+      break
+    case "div":
+      result = a / b
+      break
+    default:
+      result = "I don't understand the operator"
+  }
+  return result
+}
+
+console.log(calculator(5, 7, "add"))
+console.log(calculator(876, 86, "multi"))
+
+function setTitle (newTitle) {
+  document.querySelector("h1").textContent = newTitle
+}
